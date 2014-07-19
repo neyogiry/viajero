e8e815dc218b43e5cfd62c8b3132f806beb0635b
neyogiry <yrigoyen.fernando@gmail.com>
2014-07-19 00:45:09 -0500

    Update viajero.pl

--- Prolog/viajero.pl
+++ Prolog/viajero.pl
@@ -1,36 +1,36 @@
 
-%C
+%Ciudades
 ciudad(housband).
 ciudad(benyork).
 ciudad(austin).
 ciudad(abile).
 ciudad(winter).
 
-%d_h
+%distancia_housband
 distancia(housband, benyork, 242).
 distancia(housband, austin, 163).
 distancia(housband, abile, 352).
 distancia(housband, winter, 183).
 
-%d_b
+%distancia_benyork
 distancia(benyork, housband, 242).
 distancia(benyork, austin, 203).
 distancia(benyork, abile, 187).
 distancia(benyork, winter, 97).
 
-%d_au
+%distancia_austin
 distancia(austin, housband, 163).
 distancia(austin, benyork, 203).
 distancia(austin, abile, 216).
 distancia(austin, winter, 106).
 
-%d_ab
+%distancia_abile
 distancia(abile, housband, 352).
 distancia(abile, benyork, 187).
 distancia(abile, austin, 216).
 distancia(abile, winter, 186).
 
-%d_w
+%distancia_winter
 distancia(winter, housband, 183).
 distancia(winter, benyork, 97).
 distancia(winter, austin, 106).
@@ -43,7 +43,7 @@ recorrido(A,B,C):- distancia(A,B,C).
 recorrido(A,B,C):- distancia(A,B,C),
 				recorrido(X,Y,Z),mayor(C,Z,M).
 
-%con
+%consultas
 ?- recorrido(A,B,C),write("La distancia mas corta desde "),
 				write(A),write(" hasta "), write(B),
 				write(" es "),write(Z),nl.
